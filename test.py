c757dfd67adb4fef69207567b991c349bae2b774
Alexander Galea <agalea91@gmail.com>
2016-03-23 00:30:16 -0600

    First commit

---
+++ test.py
@@ -0,0 +1,6 @@
+from city_to_state import city_to_state_dict
+
+print('Number of cities =', len(city_to_state_dict.keys()))
+print(city_to_state_dict['Los Angeles'])
+print(city_to_state_dict['Ennis'])
+print(city_to_state_dict['Long Branch'])
